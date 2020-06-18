ca18b5d7e8ed2da0d642c3850ced44a01f397dc3
ramkumar2098 <ramkumar2098@protonmail.com>
2020-06-18 13:19:35 +0530

    Updates

---
+++ precache-manifest.3cb52a5bc459f1fa17583f8bb87e3259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25cbb02c4d2f53e053fc8e27b481f5f0",
+    "url": "/form-validation/index.html"
+  },
+  {
+    "revision": "84c22191a565f2e2f918",
+    "url": "/form-validation/static/css/main.54d3a381.chunk.css"
+  },
+  {
+    "revision": "9acaca7aec822b8433bd",
+    "url": "/form-validation/static/js/2.2108adb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form-validation/static/js/2.2108adb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c22191a565f2e2f918",
+    "url": "/form-validation/static/js/main.76d47e26.chunk.js"
+  },
+  {
+    "revision": "de7bce65bdf6c33e3ff3",
+    "url": "/form-validation/static/js/runtime-main.9f0a6d72.js"
+  }
+]);
