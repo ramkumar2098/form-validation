cd8b00784d10aea6f8b43900f576b945fc7d2665
ramkumar2098 <ramkumar2098@protonmail.com>
2020-06-18 17:11:53 +0530

    added media query

--- src/components/Label.js
+++ src/components/Label.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import style from './Detail.module.css';
 
 function Label({ id, label }) {
-  return <label htmlFor={id}>{label}</label>;
+  return (
+    <label htmlFor={id} className={style.label}>
+      {label}
+    </label>
+  );
 }
 
 export default Label;
